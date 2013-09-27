2fa9ccea26b36ed127efc44328a3eeaaa3cf3433
Nekoloch <nekoloch@gmail.com>
2013-09-27 14:29:47 -0400

    First addition (planning)

---
+++ snowfox_development_build.lua
@@ -0,0 +1,10 @@
+-- SNOWFOX
+-- Developed by Nekoloch
+
+-- Protocol Documentation: 
+
+local cli_version = "Version 0.1.0 Beta - Initial Release"
+local serv_version = "Version 0.1.0 Beta - Initial Release"
+local protocol_version = "SnowNet Protocol Version 0.1.0 Beta - Initial Release"
+
+snownet = {}
